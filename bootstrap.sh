74b9445a9be721e7080c8a797796155af4f1e5bd
Ryo Yamamoto <email@ryoyamamoto.jp>
2016-01-13 06:37:20 +0900

    Fix "warning: rpmts_HdrFromFdno: Header V3 RSA/SHA256 Signature"

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 yum install -y httpd
 yum install -y mysql-server
 yum install -y php
 yum install -y epel-release
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
 yum install -y phpMyAdmin
 ln -s /mnt/fuelphp /home/vagrant/fuelphp
 cp /mnt/fuelphp/vagrant/*.conf /etc/httpd/conf.d
